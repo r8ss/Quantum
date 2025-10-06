160e197226c6f2fa8835323ffd801463265094c8
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-10-06 21:20:59 +0600

    Silently install required packages.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Linux setup:
-apt update -y && apt upgrade -y
-apt install -y p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-pip
+# Linux setup. (silent)
+apt update -y > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1
+apt install -y p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-pip > /dev/null 2>&1
 
-# Installing Python Packages:
-pip3 install liblp google-api-python-client google-auth-httplib2 google-auth-oauthlib tgcrypto pyrogram
-pip3 install git+https://github.com/martinetd/samloader.git
+# Installing Python packages. (silent)
+pip3 install liblp google-api-python-client google-auth-httplib2 google-auth-oauthlib tgcrypto pyrogram > /dev/null 2>&1
+pip3 install git+https://github.com/martinetd/samloader.git > /dev/null 2>&1
