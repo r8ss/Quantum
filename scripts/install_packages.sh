27d7caa72d6776c0e3d006a1740b12ef4c902d87
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-10-06 22:47:08 +0600

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -7,3 +7,9 @@ apt install -y p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-p
 # Installing Python packages. (silent)
 pip3 install liblp google-api-python-client google-auth-httplib2 google-auth-oauthlib tgcrypto pyrogram
 pip3 install git+https://github.com/martinetd/samloader.git
+
+# Cleanup.
+sudo apt clean
+rm -rf ~/.cache/*
+sudo apt autoclean
+sudo apt autoremove -y
