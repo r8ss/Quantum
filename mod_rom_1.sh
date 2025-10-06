c507162d5e7964a4f35a5688d919eb1eeba7b8b8
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-10-07 01:00:57 +0600

    Update mod_rom_1.sh

--- mod_rom_1.sh
+++ mod_rom_1.sh
@@ -53,15 +53,3 @@ echo "--- Compressing .img files in $OUT_DIR ---"
 for i in "$OUT_DIR"/*.img; do [ -e "$i" ] && 7z a -mx9 "${i%.*}.img.xz" "$i"; done
 rm -rf "$OUT_DIR"/*.img
 
-echo "--- Splitting files if needed ---"
-for file in "$OUT_DIR"/*; do
-  [ -f "$file" ] || continue
-
-  FILE_SIZE=$(stat -c%s "$file")
-  if [ "$FILE_SIZE" -gt "$GIT_SUPPORT_SIZE" ]; then
-    echo "📤 Splitting: $file (Size: $FILE_SIZE bytes)"
-    split -b "$GIT_SUPPORT_SIZE" -d -a 3 "$file" "${file}."
-    rm -f "$file"
-  fi
-done
-
