f7d05265a0d472fd8779610b26e55a4080f1be2e
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-12-09 23:22:58 +0600

    Update Source_Rom.sh

--- QuantumROM/Devices/Source_Rom.sh
+++ QuantumROM/Devices/Source_Rom.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: bash $0 MODEL CSC IMEI"
+    exit 1
+fi
+
+MODEL=$1
+CSC=$2
+IMEI=$3
+
+FW_DIR="fw_download"
+BIN_DIR="bin"
+WORK_DIR="work"
+OUT_DIR="out"
+
+echo "--- Downloading $MODEL $CSC firmware ---"
+chmod +x ./scripts/download_firmware.sh
+bash ./scripts/download_firmware.sh "$MODEL" "$CSC" "$IMEI" "$FW_DIR" "$MODEL"
+
+echo ""
+echo "--- Extracting Firmware ---"
+chmod +x ./scripts/extract_firmware.sh
+bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}.zip"
+
+echo "--- Disabling Security ---"
+chmod +x ./QuantumROM/mods/security_disabler.sh
+chmod +x ./QuantumROM/mods/musti_disabler.sh
+bash ./QuantumROM/mods/security_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
+bash ./QuantumROM/mods/musti_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
