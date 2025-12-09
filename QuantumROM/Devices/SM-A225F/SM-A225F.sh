ae93261d9ff21441a684f625fe763beb12a9b9ac
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-12-09 21:11:43 +0600

    Refactor firmware download method in SM-A225F.sh
    Updated firmware download process to use a script.

--- QuantumROM/Devices/SM-A225F/SM-A225F.sh
+++ QuantumROM/Devices/SM-A225F/SM-A225F.sh
@@ -4,5 +4,11 @@ MODEL=SM-A225F
 CSC=BKD
 IMEI=350167020473859
 
-# Download stock firmware.
-sudo bash make_rom.sh $MODEL $CSC $IMEI
+FW_DIR="fw_download"
+BIN_DIR="bin"
+WORK_DIR="work"
+OUT_DIR="out"
+
+echo "--- Downloading $MODEL $CSC firmware ---"
+chmod +x ./scripts/download_firmware.sh
+bash ./scripts/download_firmware.sh "$MODEL" "$CSC" "$IMEI" "$FW_DIR" "$MODEL"
