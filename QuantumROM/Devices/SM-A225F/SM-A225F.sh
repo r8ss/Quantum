99a2c9526fb10d2bbf321e8f4da006d23bc218e0
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-12-10 16:51:00 +0600

    Update SM-A225F.sh

--- QuantumROM/Devices/SM-A225F/SM-A225F.sh
+++ QuantumROM/Devices/SM-A225F/SM-A225F.sh
@@ -13,7 +13,11 @@ echo "--- Downloading $MODEL $CSC firmware ---"
 chmod +x ./scripts/download_firmware.sh
 bash ./scripts/download_firmware.sh "$MODEL" "$CSC" "$IMEI" "$FW_DIR" "$MODEL"
 
-echo "--- Disabling Security ---"
+echo "--- Extracting $MODEL $CSC firmware ---"
+chmod +x ./scripts/extract_firmware.sh
+bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}"
+
+echo "--- Disabling $MODEL Security ---"
 chmod +x ./QuantumROM/mods/security_disabler.sh
 chmod +x ./QuantumROM/mods/musti_disabler.sh
 bash ./QuantumROM/mods/security_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
