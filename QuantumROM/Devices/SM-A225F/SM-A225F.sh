eadcb0ff6d880f71dd8d049a4951026321c992db
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-12-10 23:11:41 +0600

    Remove security disabler script from execution

--- QuantumROM/Devices/SM-A225F/SM-A225F.sh
+++ QuantumROM/Devices/SM-A225F/SM-A225F.sh
@@ -21,7 +21,5 @@ bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}.zip"
 
 echo ""
 echo "--- Disabling $MODEL Security ---"
-chmod +x ./QuantumROM/mods/security_disabler.sh
 chmod +x ./QuantumROM/mods/musti_disabler.sh
-# bash ./QuantumROM/mods/security_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
-# bash ./QuantumROM/mods/musti_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
+bash ./QuantumROM/mods/musti_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
