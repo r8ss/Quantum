a86459b55ef163b32a620e103a93872eac410382
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-12-10 16:51:55 +0600

    Add firmware download and extraction steps

--- QuantumROM/Devices/SM-A225F/SM-A225F.sh
+++ QuantumROM/Devices/SM-A225F/SM-A225F.sh
@@ -9,14 +9,17 @@ BIN_DIR="bin"
 WORK_DIR="work"
 OUT_DIR="out"
 
+echo ""
 echo "--- Downloading $MODEL $CSC firmware ---"
 chmod +x ./scripts/download_firmware.sh
 bash ./scripts/download_firmware.sh "$MODEL" "$CSC" "$IMEI" "$FW_DIR" "$MODEL"
 
+echo ""
 echo "--- Extracting $MODEL $CSC firmware ---"
 chmod +x ./scripts/extract_firmware.sh
 bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}"
 
+echo ""
 echo "--- Disabling $MODEL Security ---"
 chmod +x ./QuantumROM/mods/security_disabler.sh
 chmod +x ./QuantumROM/mods/musti_disabler.sh
