e736ad904df9eb5a567aa1f4a2282cfd4e82ef8f
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-02-02 20:21:59 +0600

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,10 +5,10 @@ sudo apt update
 sudo apt install -y p7zip-full lz4 python3 python3-pip
 
 # Installing Python packages.
-pip3 install liblp google-api-python-client google-auth-httplib2 google-auth-oauthlib tgcrypto pyrogram
+pip3 install tgcrypto pyrogram
 
 # Run with sudo to avoid path problem.
-sudo pip3 install git+https://github.com/ananjaser1211/samloader.git
+pip3 install git+https://github.com/martinetd/samloader.git
 
 # Cleanup.
 sudo apt clean
