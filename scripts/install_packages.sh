27396d0ed199f8d2cbaf1a1d4bb8b942957f168b
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-02-03 17:16:28 +0600

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,10 +2,10 @@
 
 # Linux setup.
 # sudo apt update
-sudo apt install -y p7zip-full lz4 android-sdk-libsparse-utils liblp python3 python3-pip
+sudo apt install -y p7zip-full lz4 simg2img liblp python3 python3-pip
 
 # Installing Python packages.
-pip3 install tgcrypto pyrogram
+pip3 install liblp tgcrypto pyrogram
 
 # Run with sudo to avoid path problem.
 pip3 install git+https://github.com/martinetd/samloader.git
