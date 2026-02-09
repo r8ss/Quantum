9e48b3749fbb1adcc2deac723ca995dd1e058d3b
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-02-09 18:38:54 +0600

    Fix. Its stoping google apps

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -1099,7 +1099,7 @@ APPLY_FEATURES() {
 	cp -rfa "$(pwd)/QuantumROM/Mods/Apps/." "$EXTRACTED_FIRM_DIR/"
 
 	# Remove power and data usage permissions for certain apps when Power Saver and Data Saver are always enabled.
-	sed -i '/^[[:space:]]*<allow-in-power-save/d; /^[[:space:]]*<allow-in-data-usage-save/d' "$EXTRACTED_FIRM_DIR/product/etc/sysconfig/"*.xml "$EXTRACTED_FIRM_DIR/system/system/etc/sysconfig/"*.xml
+	# sed -i '/^[[:space:]]*<allow-in-power-save/d; /^[[:space:]]*<allow-in-data-usage-save/d' "$EXTRACTED_FIRM_DIR/product/etc/sysconfig/"*.xml "$EXTRACTED_FIRM_DIR/system/system/etc/sysconfig/"*.xml
 }
 
 
