8c3bb5bc72065900c3f5730f0be893b8b6d2ce44
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-02-14 11:45:38 +0600

    Modify extract.erofs command to show output
    Remove output redirection from extract.erofs command.

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -236,7 +236,7 @@ EXTRACT_FIRMWARE_IMG() {
                 IMG_SIZE=$(stat -c%s -- "$imgfile")
                 echo "$imgfile Detected $fstype. Size: $IMG_SIZE bytes."
                 echo "Extracting $imgfile in $FIRM_DIR/$partition"
-                $(pwd)/bin/erofs-utils/extract.erofs -i "$imgfile" -x -f -o "$FIRM_DIR" >/dev/null 2>&1
+                $(pwd)/bin/erofs-utils/extract.erofs -i "$imgfile" -x -f -o "$FIRM_DIR"
                 ;;
             *)
                 echo "[$imgfile] Unknown filesystem type ($fstype), skipping"
